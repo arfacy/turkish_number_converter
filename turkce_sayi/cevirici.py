f00c19fa5ced422efb22a87377342ea4e73c0e7f
Arif <arifacay06@gmail.com>
2025-04-21 16:10:44 +0300

    has changed cevirici.py

--- turkce_sayi/cevirici.py
+++ turkce_sayi/cevirici.py
@@ -40,3 +40,53 @@ def text_to_number(text):
     pattern = r"(sıfır|bir|iki|üç|dört|beş|altı|yedi|sekiz|dokuz|on|yirmi|otuz|kırk|elli|altmış|yetmiş|seksen|doksan|yüz|bin)(\\s+(bir|iki|üç|dört|beş|altı|yedi|sekiz|dokuz))?"
 
     return re.sub(pattern, replace_number_words, text)
+
+
+
+
+
+# import re
+
+# def text_to_number(text):
+#     birler = {
+#         "bir": 1, "iki": 2, "üç": 3, "dört": 4, "beş": 5,
+#         "altı": 6, "yedi": 7, "sekiz": 8, "dokuz": 9, "sıfır": 0
+#     }
+#     onlar = {
+#         "on": 10, "yirmi": 20, "otuz": 30, "kırk": 40,
+#         "elli": 50, "altmış": 60, "yetmiş": 70,
+#         "seksen": 80, "doksan": 90
+#     }
+
+#     def parse_number(words):
+#         total = 0
+#         current = 0
+#         for word in words:
+#             if word in birler:
+#                 current += birler[word]
+#             elif word in onlar:
+#                 current += onlar[word]
+#             elif word == "yüz":
+#                 if current == 0:
+#                     current = 100
+#                 else:
+#                     current *= 100
+#             elif word == "bin":
+#                 if current == 0:
+#                     current = 1000
+#                 else:
+#                     current *= 1000
+#                 total += current
+#                 current = 0
+#         return str(total + current)
+
+#     def replace_number_words(match):
+#         kelimeler = match.group(0).split()
+#         return parse_number(kelimeler)
+
+#     pattern = r"(sıfır|bir|iki|üç|dört|beş|altı|yedi|sekiz|dokuz|on|yirmi|otuz|kırk|elli|altmış|yetmiş|seksen|doksan|yüz|bin)(\\s+(bir|iki|üç|dört|beş|altı|yedi|sekiz|dokuz))?"
+
+#     return re.sub(pattern, replace_number_words, text)
+
+
+
